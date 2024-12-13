ab317ad1ebd3c8faa76f6544fb897e1f993cf98f
t-okuaki <t-okuaki@nekojarashi.com>
2024-12-13 13:25:12 +0900

    Add: Prettier で Tailwind クラスを自動ソートできるようにする
    prettier-plugin-tailwindcss を追加し、以下を実行した。
    
    docker compose run --rm frontend yarn prettier . --write

--- frontend/.prettierrc.js
+++ frontend/.prettierrc.js
@@ -1,1 +1,12 @@
-module.exports = {};
+/**
+ * Prettier の設定ファイルです。ドキュメントは以下です。
+ * https://prettier.io/docs/en/configuration.html
+ */
+module.exports = {
+  plugins: [
+    // Tailwind CSS のクラス名をソートするプラグインです。
+    // VSCode の esbenp.prettier-vscode 拡張機能の保存時自動整形（formatOnSave）で動作しない場合、
+    // VSCode の再起動を試してください。
+    "prettier-plugin-tailwindcss",
+  ],
+};
